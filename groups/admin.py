91ee687cf94e527a6c5fdacac0b31042e6372b3c
zacky ffx <zacky ffx>
2017-07-31 08:29:43 +0800

    added functionality

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from. import models
 
-# Register your models here.
+admin.site.register(models.Group)
+
+
+class GroupMemberInline(admin.TabularInLine):
+    model = models.GroupMember
